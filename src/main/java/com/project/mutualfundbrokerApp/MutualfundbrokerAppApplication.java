12fc0a02e8c5e9959ea718f82422342232a9f98c
Mohammed Asif Khan <MohammedAsif.Khan@target.com>
2025-01-04 02:38:51 +0530

    Initial commit

---
+++ src/main/java/com/project/mutualfundbrokerApp/MutualfundbrokerAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.mutualfundbrokerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutualfundbrokerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutualfundbrokerAppApplication.class, args);
+	}
+
+}
